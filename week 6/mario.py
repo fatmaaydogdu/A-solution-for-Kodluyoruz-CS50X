a6f0f896471ecd9a9e08f1089dd66aaefb357fc8
Fatma Aydoğdu <56413015+fatmaaydogdu@users.noreply.github.com>
2021-02-17 00:15:53 +0300

    Add files via upload

---
+++ week 6/mario.py
@@ -0,0 +1,17 @@
+
+def harfYaz(harf, tekrar):
+    for j in range(tekrar):
+        print(harf,end="")
+
+yukseklik = get_int("Yukseklik:")
+while yukseklik < 1 or yukseklik > 8:
+    yukseklik = get_int("Yukseklik:")
+yildiz_sayisi = 1
+bosluk_sayisi = yukseklik-1
+
+for i in range(yukseklik):
+    harfYaz(" ",bosluk_sayisi)
+    bosluk_sayisi -= 1
+    harfYaz("#",yildiz_sayisi)
+    yildiz_sayisi += 1
+    print()
